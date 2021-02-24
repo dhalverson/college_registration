f878665de9c8835aae002d97e9a6f33d17b334d5
Daniel Halverson <63027888+dhalverson@users.noreply.github.com>
2021-02-24 15:23:21 -0700

    Adds seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+CourseStudent.destroy_all
+Student.destroy_all
+Course.destroy_all
+
+ActiveRecord::Base.connection.tables.each do |t|
+  ActiveRecord::Base.connection.reset_pk_sequence!(t)
+end
+
+student_01 = Student.create!(name: Faker::Name.name, email: Faker::Internet.email)
+student_02 = Student.create!(name: Faker::Name.name, email: Faker::Internet.email)
+student_03 = Student.create!(name: Faker::Name.name, email: Faker::Internet.email)
+
+course_01 = Course.create!(name: Faker::Educator.course_name, time: '8:00')
+course_02 = Course.create!(name: Faker::Educator.course_name, time: '11:00')
+course_03 = Course.create!(name: Faker::Educator.course_name, time: '14:00')
+
+course_student_01 = CourseStudent.create!(course: course_01, student: student_01)
+course_student_02 = CourseStudent.create!(course: course_02, student: student_02)
+course_student_03 = CourseStudent.create!(course: course_03, student: student_03)
+course_student_04 = CourseStudent.create!(course: course_03, student: student_01)
+
+print 'Seeding Successful!'
