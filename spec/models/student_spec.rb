38e8458f5b7dc86936be2a684635b709fe2f6a93
Daniel Halverson <63027888+dhalverson@users.noreply.github.com>
2021-02-24 14:55:56 -0700

    Adds student relationship validations, and tests passing

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -8,5 +8,6 @@ RSpec.describe Student, type: :model do
 
   describe 'relationships' do
     it { should have_many :course_students }
+    it { should have_many(:courses).through(:course_students) }
   end
 end
